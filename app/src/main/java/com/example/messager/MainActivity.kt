558715e8569759233ba386e31746f8aa77298e52
Federico Larrosa <federico.gabriel.larrosa@gmail.com>
2023-04-18 00:12:44 -0300

    inicial commit

--- app/src/main/java/com/example/messager/MainActivity.kt
+++ app/src/main/java/com/example/messager/MainActivity.kt
@@ -34,6 +34,7 @@ class MainActivity : ComponentActivity() {
                     ) {
                         // prueba
                         //prueba2
+                        //
                        // UserList()
                         ChatScreen(Modifier.padding(PaddingValues)/*,messages = messages*/)
                     }
